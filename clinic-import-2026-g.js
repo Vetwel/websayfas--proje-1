b1a4d040d46316f9691b8f9592acc07df5dea662
Vetwel <noben@vedaturkiye.com>
2026-08-21 23:04:09 +0300

    Add MyVet Veteriner Kliniği to clinic directory

--- clinic-import-2026-g.js
+++ clinic-import-2026-g.js
@@ -1,2 +1,5 @@
 // VetWel clinic import 2026-08-21 part 7
-window.VETWEL_MERGE_CLINICS([["FENERYOLU VETERİNER POLİKLİNİĞİ","İstanbul","Kadıköy","FENERYOLU MAH. BAĞDAT CAD. NO:103/D","05362001930"]]);
+window.VETWEL_MERGE_CLINICS([
+  ["FENERYOLU VETERİNER POLİKLİNİĞİ","İstanbul","Kadıköy","FENERYOLU MAH. BAĞDAT CAD. NO:103/D","05362001930"],
+  ["MYVET VETERİNER KLİNİĞİ","İstanbul","Beşiktaş","VİŞNEZADE MAH. REFİK OSMAN TOP SOK. NO.13 D.1A","02122591191"]
+]);

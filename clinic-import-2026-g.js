1e90bf807fdcaa5e2714604c35142de19ae5c4fb
Vetwel <noben@vedaturkiye.com>
2026-08-21 22:40:03 +0300

    Add Feneryolu Veteriner Polikliniği

---
+++ clinic-import-2026-g.js
@@ -0,0 +1,2 @@
+// VetWel clinic import 2026-08-21 part 7
+window.VETWEL_MERGE_CLINICS([["FENERYOLU VETERİNER POLİKLİNİĞİ","İstanbul","Kadıköy","FENERYOLU MAH. BAĞDAT CAD. NO:103/D","05362001930"]]);

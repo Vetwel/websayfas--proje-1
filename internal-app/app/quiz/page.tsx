62405e9c450bd25440fff6008755dcd492ec8f8a
Vetwel <noben@vedaturkiye.com>
2026-08-26 11:32:51 -0400

    Use vinext-safe auth in onboarding quiz

--- internal-app/app/quiz/page.tsx
+++ internal-app/app/quiz/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { auth } from "@clerk/nextjs/server";
+import { auth } from "@/lib/clerk-server";
 import { redirect } from "next/navigation";
 import { isClerkConfigured } from "@/lib/internal-config";
 import QuizClient from "./quiz-client";

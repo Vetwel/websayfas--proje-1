2244c21c6bd77cd0cf4f9163eddf3410205274b9
Vetwel <noben@vedaturkiye.com>
2026-08-26 11:34:55 -0400

    Use vinext-safe auth in training detail

--- internal-app/app/training/[slug]/page.tsx
+++ internal-app/app/training/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { auth } from "@clerk/nextjs/server";
+import { auth } from "@/lib/clerk-server";
 import { notFound, redirect } from "next/navigation";
 import { isClerkConfigured } from "@/lib/internal-config";
 import { getTrainingModule } from "@/lib/training-content";

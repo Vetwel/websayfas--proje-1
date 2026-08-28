8b085e139df6a1df9bf61309218b13621ad5ebf4
Vetwel <noben@vedaturkiye.com>
2026-08-28 16:17:20 -0400

    Add Patiliva Animare and Vetmahal clinics

---
+++ clinic-import-2026-h.js
@@ -0,0 +1,6 @@
+// VetWel clinic import 2026-08-28 part 8
+window.VETWEL_MERGE_CLINICS([
+  ["PATİLİVA VETERİNER KLİNİĞİ","Kocaeli","Darıca","CAMİ MAH. ALİ ARICAN CAD. B BLOK NO.11A","05337221448"],
+  ["ANİMARE VETERİNER KLİNİĞİ","İstanbul","Ümraniye","ÇAMLIK MAH. ŞENOL GÜNEŞ BULV. NAZ APT. NO:30/1","05074039352"],
+  ["VETMAHAL VETERİNER KLİNİĞİ","İstanbul","Üsküdar","KISIKLI MAH. HANIMSETİ SK. NO39/1","05053752285"]
+]);
